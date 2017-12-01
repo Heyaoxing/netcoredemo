dea905e7a6ad479b3d007c5870b5a2dd6dae7a52
heyx <730530507@qq.com>
2017-12-01 09:41:27 +0800

    1

---
+++ netcoredemo/SpiderWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace SpiderWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            try
+            {
+                Console.WriteLine("开始");
+                BaseUsage.CustmizeProcessorAndPipeline();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.Read();
+        }
+    }
+}
